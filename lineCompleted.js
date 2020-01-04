56c05b3dfb160bbb5f1c1de231c365e1bd0ba645
Robin <robinzmuda@gmail.com>
2020-01-04 12:56:49 +0100

    Collision ligne

--- lineCompleted.js
+++ lineCompleted.js
@@ -1,25 +1,53 @@
-function Checkline() {
-	for (let i = 0; i <= canvasHeigth; i += cubeTaille) {
-		for (let j = 2; j <= canvasWidth; j += cubeTaille) {
-			let hit = 0;
-			stroke(255);
-			point(j, i);
-			for (let cube of fixedCube) {
-				for (let cube2 of cube.cubes) {
-					if (collideLineRect(j, i, canvasWidth, i, cube2.x, cube2.y, cube2.x2, cube2.y2)) {
-						hit++;
-					}
-				}
-				if (hit >= 32) {
-					for (let k = fixedCube.length - 1; k > 0; k--) {
-						for (let l = fixedCube[k].cubes.length - 1; l > 0; l--) {
-							if (collideLineRect(j, i, canvasWidth, i, fixedCube[0].cubes[l].x, fixedCube[0].cubes[l].y, fixedCube[0].cubes[l].x2, fixedCube[0].cubes[l].y2)) {
-								fixedCube[k].destroyCube(l);
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+// function Checkline() {
+//   for (let i = 0; i <= canvasHeigth; i += cubeTaille) {
+//     for (let j = 2; j <= canvasWidth; j += cubeTaille) {
+//       let hit = 0;
+//       stroke(255);
+//       point(j, i);
+//       for (let cube of fixedCube) {
+//         for (let cube2 of cube.cubes) {
+//           if (collideLineRect(j, i, canvasWidth, i, cube2.x, cube2.y, cube2.x2, cube2.y2)) {
+//             hit++;
+//           }
+//         }
+//         if (hit >= 32) {
+//           for (let k = fixedCube.length - 1; k > 0; k--) {
+//             for (let l = fixedCube[k].cubes.length - 1; l >= 0; l--) {
+//               if (collideLineRect(j, i, canvasWidth, i, fixedCube[0].cubes[l].x, fixedCube[0].cubes[l].y, fixedCube[0].cubes[l].x2, fixedCube[0].cubes[l].y2)) {
+//                 fixedCube[k].destroyCube(l);
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+//
+// }
+
+function lineComplete() {
+  for (let i = cubeTaille / 2; i <= canvasHeigth; i += cubeTaille) {
+    stroke(255, 0, 0);
+    line(0, i, canvasWidth, i);
+    let hit = 0;
+    for (let cube of fixedCube) {
+      for (let cube2 of cube.cubes) {
+        if (collideLineRect(0, i, canvasWidth, i, cube2.x, cube2.y, cube2.x2, cube2.y2))
+          hit++;
+      }
+    }
+    if (hit == 16) {
+      hit = 0;
+      console.log(hit + " coll");
+      for (let cube of fixedCube) {
+        for (let j = cube.cubes.length - 1; j >= 0; j--) {
+          if (collideLineRect(0, i, canvasWidth, i, cube.cubes[j].x, cube.cubes[j].y, cube.cubes[j].x2, cube.cubes[j].y2)) {
+            console.log("testa");
+            cube.destroyCube(j);
+          }
+        }
+      }
+
+    }
+  }
 }
--- sketch.js
+++ sketch.js
@@ -1,49 +1,58 @@
 function setup() {
-	createCanvas(canvasWidth, canvasHeigth)
-	Cube = new bigCube(0, 0);
-	slider = createSlider(0, 200, 1);
+  createCanvas(canvasWidth, canvasHeigth);
+  Cube = new bigCube(0, 0);
+  slider = createSlider(0, 200, 1);
 }
 
 function draw() {
-	background(0);
-	for (let i = 0; i < slider.value(); i++) {
-		compteur++;
-		if (Cube.descente == 0) {
-			fixedCube.push(Cube);
-			console.log(fixedCube[0].cubes.length);
-			Cube = new bigCube(0, 0);
-			for (let cube of fixedCube) {
-				if (cube.collisionEnd()) {
-					slider = 0;
-				}
-			}
-			Checkline();
-		}
-		if (compteur % 50 == 0) {
-			Cube.descendre();
-			Cube.collision();
-		}
+  <
+  background(0);
+  for (let i = 0; i < slider.value(); i++) {
+    compteur++;
+    if (Cube.descente == 0) {
+      fixedCube.push(Cube);
+      let rand = random(0, 4);
+      if (rand < 1) {
+        Cube = new bigCube(0, 0);
+      } else if (rand < 2) {
+        Cube = new bigLine(0, 0);
+      } else if (rand < 3) {
+        Cube = new bigPointe(0, 0);
+      } else if (rand < 4) {
+        Cube = new bigL(0, 0);
+      }
+      for (let cube of fixedCube) {
+        if (cube.collisionEnd()) {
+          slider = 0;
+        }
+      }
+      lineComplete();
+    }
+    if (compteur % 50 == 0) {
+      Cube.descendre();
+      Cube.collision();
+    }
 
-		for (let cube of fixedCube) {
-			for (let cube2 of cube.cubes) {
-				Cube.collisionObjet(cube2.x, cube2.y, cube2.x2, cube2.y2);
-			}
-		}
-	}
+    for (let cube of fixedCube) {
+      for (let cube2 of cube.cubes) {
+        Cube.collisionObjet(cube2.x, cube2.y, cube2.x2, cube2.y2);
+      }
+    }
+  }
 
-	if (rightPressed) {
-		Cube.moveDroite();
-	}
-	if (leftPressed) {
-		Cube.moveGauche();
-	}
-	if (upPressed) {
-		Cube.descendre();
-		Cube.collision();
-	}
-	//DRAWING
-	Cube.draw();
-	for (let cube of fixedCube) {
-		cube.draw();
-	}
+  if (rightPressed) {
+    Cube.moveDroite();
+  }
+  if (leftPressed) {
+    Cube.moveGauche();
+  }
+  if (upPressed) {
+    Cube.descendre();
+    Cube.collision();
+  }
+  //DRAWING
+  Cube.draw();
+  for (let cube of fixedCube) {
+    cube.draw();
+  }
 }
